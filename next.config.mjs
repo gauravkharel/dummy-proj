043d568068eb5bca144c59ec36ea1192b2a8813e
gauravkharel <kharel619@gmail.com>
2024-03-02 23:34:00 +0545

    Product list component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [{
+                protocol: 'https',
+                hostname: 'cdn.dummyjson.com',
+                port: '',
+                pathname: '/product-images/**'
+            }]
+    }
+};
 
 export default nextConfig;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -10,18 +10,37 @@ import {
     PaginationPrevious,
 } from "@/components/ui/Pagination"
 import Link from 'next/link';
+import Image from 'next/image';
+import { AspectRatio } from '@/components/ui/AspectRatio';
+import { Card } from './ui/Card';
+
 
 const Products = ({ }) => {
     const [serverUrl, setServerUrl] = useState("https://dummyjson.com/products");
     const { data, isPending, error, refetch, undofetch } = useFetch(serverUrl);
+
+    function discountedAmout(price, discountPercentage) {
+        const newAmount = Math.round(price - (price * discountPercentage / 100))
+        return newAmount
+    }
+
     return (
         <div>
             {isPending && <div>Loading....</div>}
             {error && <div>{error}</div>}
-            <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8'>
                 {!isPending && !error && data && data.products.map((product) =>
                     <Link key={product.id} href={`/product/${product.id}`}>
-                        {product.title}
+                        <AspectRatio ratio={16 / 9} >
+                            <Image src={product.thumbnail} alt={product.description} fill={true} />
+                        </AspectRatio>
+                        <div className='pt-2'>
+                            <span className='font-medium text-lg'>{product.brand}  </span>
+                            <span className='px-2 py-3 bg-blue-100 text-gray-400 border-slate font-medium text-xs rounded-lg'>{product.category}</span> <br />
+                            <span>{product.title}</span> <br />
+                            <span className='line-through'>${product.price}</span>
+                            <span className='font-medium text-lg'> ${discountedAmout(product.price, product.discountPercentage)}</span>
+                        </div>
                     </Link>
                 )}
             </div>
@@ -30,7 +49,6 @@ const Products = ({ }) => {
                     <PaginationItem>
                         <PaginationPrevious aria-disabled onClick={undofetch} />
                     </PaginationItem>
-
                     <PaginationItem>
                         <PaginationNext onClick={refetch} />
                     </PaginationItem>
